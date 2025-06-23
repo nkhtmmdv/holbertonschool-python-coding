63775282aa8ade86c2a7151684e11262a332ccca
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-23 12:15:14 +0000

    1

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""Main file to test the Square class."""
-
 Square = __import__('0-square').Square
 
 my_square = Square(3)
--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""This module defines a Square class with a private size attribute."""
-
 class Square:
-    """Represents a square defined by its size."""
-
     def __init__(self, size):
-        """Initialize the square with the given size (no validation)."""
         self.__size = size
