126187cccd3b33302ed833376b9fa17c616d0817
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-23 12:04:28 +0000

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-#smth
+"""
+This module defines the Square class with a private size attribute.
+"""
+
 class Square:
-    def __init__(self,size):
+    def __init__(self, size):
         self.__size = size
