8eaa176f04c6f027de57bff996252a15624f146e
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-24 10:02:59 +0000

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,15 +3,19 @@
 
 
 class Square:
+    '''Represents a square with size property.'''
+
     def __init__(self, size=0):
         self.size = size  # Uses the setter for validation
 
     @property
     def size(self):
+        '''Getter for the size attribute.'''
         return self.__size
 
     @size.setter
     def size(self, value):
+        '''Setter for the size attribute with validation.'''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -19,4 +23,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        '''Returns the current square area.'''
         return self.__size ** 2
