bf7c672ede2a2e70e1a8088af446ac3035bcd011
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-24 09:29:45 +0000

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
-This module defines the Square class.
-"""
+'''This module defines the Square class.'''
+
+
 class Square:
-    def __init__(self,size=0):
-        if type(size) != int:
+    ''' 0 '''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
